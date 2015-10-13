e850f930f9b6d6836de85d28a1558ab924458f6f
Zachary Hensley <zachary.hensley@metova.com>
2015-10-12 21:12:59 -0500

    Began adding more models (Category / Transaction) and began hooking their relationships up properly. Updated view endpoint for /budget to reflect new models.

--- app/models.py
+++ app/models.py
@@ -3,8 +3,9 @@ from . import db
 # Models
 class Budget(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(40))
+    title = db.Column(db.String(128))
     amount = db.Column(db.Integer)
+    categories = db.relationship('Category', backref='budget', lazy='dynamic')
 
     def __init__(self, title, amount):
         self.title = title
@@ -16,5 +17,60 @@ class Budget(db.Model):
        return {
            'id': self.id,
            'title': self.title,
-           'amount': self.amount
+           'amount': self.amount,
+           'categories': [i.serialize for i in self.categories]
+       }
+
+
+class CategoriesTransactions(db.Model):
+    __tablename__ = 'categories_transactions'
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), primary_key=True)
+    transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.id'), primary_key=True)
+    inflow = db.Column(db.Float)
+    outflow = db.Column(db.Float)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    amount = db.Column(db.Float)
+    budgetId = db.Column(db.Integer, db.ForeignKey('budget.id'))
+    transactions = db.relationship('CategoriesTransactions', backref='category')
+    # backref adds .category to join table
+
+    def __init__(self, title, amount):
+        self.title = title
+        self.amount = amount
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'title': self.title,
+           'amount': self.amount,
+           'budgetId': self.budgetId,
+           'transactions': [i.transaction.serialize(i) for i in self.transactions]
+       }
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    to = db.Column(db.String(128))
+    notes = db.Column(db.String(256))
+    createdAt = db.Column(db.Date)
+    categories = db.relationship('CategoriesTransactions', backref='transaction')
+    # backref adds .transaction to join table
+
+    def __init__(self, to, notes, createdAt):
+        self.to = to
+        self.notes = notes
+        self.createdAt = createdAt
+
+    def serialize(self, assoc):
+       """Return object data in easily serializeable format"""
+       return {
+           'id': self.id,
+           'to': self.to,
+           'notes': self.notes,
+           'createdAt': self.createdAt,
+           'outflow': assoc.outflow,
+           'inflow': assoc.inflow
        }
--- runserver.py
+++ runserver.py
@@ -1,26 +1,64 @@
 from app import create_app, db
-from app.models import Budget
+from app.models import Budget, Transaction, Category, CategoriesTransactions
+from datetime import datetime
 
 app = create_app()
 
-@app.cli.command('seedBudgets')
-def seedBudgets():
-    """Seed the db with budgets"""
+@app.cli.command('seedDb')
+def seedDb():
+    """Seed the db"""
 
     db.create_all()
 
+    # Budgets
     budget1 = Budget("Budget 1", 5000)
-    budget2 = Budget("Budget 2", 7000)
-    budget3 = Budget("Budget 3", 2000)
-    budget4 = Budget("Budget 4", 1500)
-
     db.session.add(budget1)
-    db.session.add(budget2)
-    db.session.add(budget3)
-    db.session.add(budget4)
+
+    # Categories
+    category1 = Category("Category 1", 200.00)
+    category2 = Category("Category 2", 800.00)
+    db.session.add(category1)
+    db.session.add(category2)
+
+    # Budget-Category links
+    budget1.categories.append(category1)
+    budget1.categories.append(category2)
+
+
+    # Transactions
+    transaction1 = Transaction("Business 1", "Outflow transaction", datetime.now())
+    transaction2 = Transaction("Business 2", "Inflow transaction", datetime.now())
+    transaction3 = Transaction("Business 3", "Split transaction", datetime.now())
+    db.session.add(transaction1)
+    db.session.add(transaction2)
+    db.session.add(transaction3)
+
+    # CategoriesTransactions links (many-to-many)
+    link1 = CategoriesTransactions(outflow=25.00)
+    link1.category = category1
+    link1.transaction = transaction1
+
+    link2 = CategoriesTransactions(inflow=10.00)
+    link2.category = category2
+    link2.transaction = transaction2
+
+    link3 = CategoriesTransactions(outflow=50.00)
+    link3.category = category1
+    link3.transaction = transaction3
+
+    link4 = CategoriesTransactions(outflow=50.00)
+    link4.category = category2
+    link4.transaction = transaction3
+
+
+    category1.transactions.append(link1)
+    category2.transactions.append(link2)
+    category1.transactions.append(link3)
+    category2.transactions.append(link4)
+
     db.session.commit()
 
-    print("Seeded the database with budgets")
+    print("Seeded the database")
 
     budgetList = Budget.query.all()
     print(budgetList)
